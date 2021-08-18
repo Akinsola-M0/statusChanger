cc7cf81dffe0e12f77d88f8e406f734fa75415a1
Akinsola-M0 <olulare6@gmail.com>
2021-08-18 22:28:09 +0100

    status changer

---
+++ app/src/main/java/com/example/myapplication/hello.java
@@ -0,0 +1,11 @@
+package com.example.myapplication;
+
+public class hello {
+
+    String name = " Moses";
+
+    public void sayHello()
+    {
+        System.out.println("hello " + name);
+    }
+}
