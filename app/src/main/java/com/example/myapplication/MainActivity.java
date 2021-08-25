98e332ce7e15ae1d99e751a462f1bf049c421871
Akinsola-M0 <olulare6@gmail.com>
2021-08-25 13:54:41 +0100

    material design UI

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,40 +1,67 @@
 package com.example.myapplication;
 
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.textfield.TextInputEditText;
+
+import java.util.ArrayList;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+    ArrayList<Integer> colorList = new ArrayList<>();
+    ArrayList<Typeface> fontList = new ArrayList<Typeface>();
+
+    Random rand = new Random();
+   ;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        colorList.add(Color.BLUE);
+        colorList.add(Color.GREEN);
+        colorList.add(Color.rgb(128,0,0));
+
+        fontList.add(Typeface.SANS_SERIF);
+        fontList.add(Typeface.MONOSPACE);
+        fontList.add(Typeface.SERIF);
+
+
         TextView textView = findViewById(R.id.textView);
-        EditText editText = findViewById(R.id.editText);
-        Button button = findViewById(R.id.button);
-        Button blueButton = findViewById(R.id.bluebutton);
-        Button greenButton = findViewById(R.id.greenbutton);
-        Button maroonButton = findViewById(R.id.maroonbutton);
+        TextInputEditText editText = findViewById(R.id.outlinedTextEdit);
+
+        ImageButton sendButton = findViewById(R.id.button);
+        ImageButton colorChange = findViewById(R.id.colorChange);
+        ImageButton textChange = findViewById(R.id.textChange);
 
 
-        button.setOnClickListener(v -> {
+        sendButton.setOnClickListener(v -> {
             String text = editText.getText().toString();
 
             textView.setText(text);
             editText.setText("");
         });
 
-        blueButton.setOnClickListener(v -> textView.setTextColor(Color.BLUE));
+        textChange.setOnClickListener(v -> {
+            int x = rand.nextInt(3);
+            textView.setTypeface(fontList.get(x));
+        });
+
+        colorChange.setOnClickListener(v -> {
+            int x = rand.nextInt(3);
+            textView.setTextColor(colorList.get(x));
+
+        });
 
 
-        greenButton.setOnClickListener(v -> textView.setTextColor(Color.GREEN));
-
-        maroonButton.setOnClickListener(v -> textView.setTextColor(Color.rgb(128,0,0)));
     }
 }
